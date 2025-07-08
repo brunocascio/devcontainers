f7399ec86eb241af1195cb8e6d791f257ae275ed
Bruno Cascio <brunocascio@gmail.com>
2025-07-08 18:50:57 -0300

    upgrade docker php installer to 2.8.4

--- features/install-php-extensions/install.sh
+++ features/install-php-extensions/install.sh
@@ -2,6 +2,6 @@
 set -e
 curl -sSLf \
     -o /usr/local/bin/install-php-extensions \
-    https://github.com/mlocati/docker-php-extension-installer/releases/download/2.2.5/install-php-extensions
+    https://github.com/mlocati/docker-php-extension-installer/releases/download/2.8.4/install-php-extensions
 chmod +x /usr/local/bin/install-php-extensions
 install-php-extensions "$EXTENSIONS"
